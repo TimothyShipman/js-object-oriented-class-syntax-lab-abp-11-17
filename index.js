6b456faf289a00b7d93038040c3d6b49fc9bc3f6
Tim Shipman <bricklayertim@yahoo.com>
2017-11-16 16:01:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ class BoardMember{
   sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`}
 }
 
-polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+let polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
